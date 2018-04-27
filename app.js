730667e803fecd5661afeb8e705fff57fd1b1bea
7summer <7summer@192.168.0.102>
2018-04-28 06:33:41 +0800

    解决电影数据无法加载的问题

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ App({
   globalData: {
     g_isPlayingMusic: false,
     g_currentMusicPostId: null,
-    doubanBase: "https://api.douban.com",
+    doubanBase: "http://t.yushu.im",
     g_userInfo: null
   }
 })
